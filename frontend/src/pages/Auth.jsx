5fc6fc23300601478bdbd21b4144fdbdf5c71ccf
Valton Kasami <valtonkasamii@outlook.com>
2025-02-16 14:48:55 +0100

    redirect

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -3,33 +3,10 @@ import { faMastodon } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Auth = () => {
 
-    const handleClick = async (e) => {
-        e.preventDefault()
-
-        try {
-            const response = await fetch("https://vyox-backend.onrender.com/login", {
-                
-            })
-            
-            if (!response.ok) {
-                const errorData = await response.json();
-                console.log(errorData)
-              } else {
-            const data = await response.json()
-            console.log(data)
-              
-                window.location = "/"
-              }
-              
-            } catch (error) {
-                console.error(error.message)
-            }
-        }
-
   return (
     <div className=' flex items-center justify-center h-[100vh]'>
 
-        <a href="http://127.0.0.1:5000/login"><button className='text-xl flex font-[600] bg-[#6114FF] px-4 py-2 rounded-full items-center justify-center'>
+        <a href="https://vyox-frontend.onrender.com/login"><button className='text-xl flex font-[600] bg-[#6114FF] px-4 py-2 rounded-full items-center justify-center'>
             <FontAwesomeIcon className='mr-2 text-3xl' icon={faMastodon}/>
             Continue with Mastodon
         </button></a>
