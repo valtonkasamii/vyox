699efeabc272acb64f96f44b360d209015e385c0
Valton Kasami <valtonkasamii@outlook.com>
2025-02-17 14:29:43 +0100

    ugh

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,7 @@ app.config.update({
     'SESSION_COOKIE_SECURE': True,
     'SESSION_COOKIE_SAMESITE': 'None',
     'SESSION_COOKIE_HTTPONLY': True,
-    'SESSION_REFRESH_EACH_REQUEST': False
+    'SESSION_REFRESH_EACH_REQUEST': True
 })
 
 # Initialize session after config
