18f6e280c3e68e1c6d4d376b847fdd21a3a17eb7
Valton Kasami <valtonkasamii@outlook.com>
2025-02-17 13:17:07 +0100

    hmm

--- backend/app.py
+++ backend/app.py
@@ -40,7 +40,6 @@ app.config.update({
     'SESSION_COOKIE_SECURE': True,
     'SESSION_COOKIE_SAMESITE': 'None',
     'SESSION_COOKIE_HTTPONLY': True,
-    'SESSION_COOKIE_DOMAIN': '.onrender.com',
     'SESSION_REFRESH_EACH_REQUEST': False
 })
 
