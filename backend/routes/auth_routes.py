c55d7ce86516b05abe436d3f2ddc0a06fcdecd27
Valton Kasami <valtonkasamii@outlook.com>
2025-02-18 21:57:26 +0100

    hhh

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,21 +1,8 @@
 from flask import Blueprint
 from controllers.auth_controller import login, callback, getme, logout
 from middleware.protect_route import authenticate_request
-from flask_cors import CORS
 
 auth_routes = Blueprint('auth', __name__)
-
-CORS(
-    auth_routes,
-    resources={
-        r"/api/*": {
-            "origins": "https://vyox.vercel.app",
-            "allow_headers": ["Accept", "Content-Type", "Origin", "Authorization"],
-            "expose_headers": ["Accept", "Content-Type", "Origin", "Authorization"]
-        }
-    },
-    supports_credentials=True
-)
 
 auth_routes.route('/api/login', methods=['GET'])(login)
 auth_routes.route('/api/callback', methods=['GET'])(callback)
--- backend/routes/posts_route.py
+++ backend/routes/posts_route.py
@@ -1,20 +1,7 @@
 from flask import Blueprint
 from controllers.posts_controller import getAllPosts
 from middleware.protect_route import authenticate_request
-from flask_cors import CORS
 
 post_routes = Blueprint('posts', __name__)
-
-CORS(
-    post_routes,
-    resources={
-        r"/api/*": {
-            "origins": "https://vyox.vercel.app",
-            "allow_headers": ["Accept", "Content-Type", "Origin", "Authorization"],
-            "expose_headers": ["Accept", "Content-Type", "Origin", "Authorization"]
-        }
-    },
-    supports_credentials=True
-)
 
 post_routes.route('/api/posts', methods=['POST'])(authenticate_request(getAllPosts))
