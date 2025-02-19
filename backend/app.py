342abd94e94a13a9410ba332a9fb376b6e5f8d11
Valton Kasami <valtonkasamii@outlook.com>
2025-02-19 02:16:41 +0100

    yee

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 CORS(
     app,
-    origins="http://localhost:5000",
+    origins="http://localhost:5173",
     allow_headers=["Accept", "Content-Type", "Origin", "Authorization"],
     expose_headers=["Accept", "Content-Type", "Origin", "Authorization"],
     supports_credentials=True
