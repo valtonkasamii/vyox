693f6f635b7309e65c9f2c892c28aeb9d8d5cea9
valtonkasamii <valtonkasamii@outlook.com>
2025-03-05 08:40:17 +0100

    Update post.py

--- backend/functions/post.py
+++ backend/functions/post.py
@@ -39,8 +39,10 @@ def fetch_multiple_old_posts(instance_url, headers, new_max_id, since_id, max_id
     
     if posts_new_boolean:
         for post in posts_new:
+            current_new_max_id = int(posts_new[-1].get('id', ''))
             if int(post.get('id', '')) >= int(max_id) and int(post.get('id', '')) <= int(since_id):
-                current_new_max_id = int(posts_new[-1].get('id', ''))
+                posts_new_boolean = False
+                current_new_max_id = None
     else:
         current_new_max_id = None
         posts_new_boolean = False 
