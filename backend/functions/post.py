8feedb67b7530df86b7fd32b2b7656023074f947
valtonkasamii <valtonkasamii@outlook.com>
2025-03-09 13:58:57 +0100

    Update post.py

--- backend/functions/post.py
+++ backend/functions/post.py
@@ -59,8 +59,10 @@ def fetch_multiple_old_posts(instance_url, headers, new_max_id, since_id, max_id
 
         for post in new_posts:
             pid = int(post.get('id', 0))
-            if pid >= int(max_id) and pid <= int(since_id):
+            if max_id and since_id and (pid >= int(max_id) and pid <= int(since_id)):
                 posts_new_boolean = False
+                break
+            else:
                 break
 
         if not posts_new_boolean:
