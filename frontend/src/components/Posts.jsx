90299b9581a2536e42486cd2878c4706a37a599f
Valton Kasami <valtonkasamii@outlook.com>
2025-06-18 15:27:09 +0200

    loading2

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -884,7 +884,7 @@ const Posts = ({profile, user, starr, single}) => {
             </div>
             </div></div>}
         {!swap2() && <div className='mb-2'></div>}
-        {((allPosts.length === 0 && !profile && !single) || (profile && profilePosts === 0)) && <div className='flex justify-center'> 
+        {loading2 && <div className='flex justify-center'> 
             <div className={`flex justify-center text-4xl px-4 pt-[6px] py-2 border-2 w-fit rounded-[15px] ${css6()} mb-3`}>
             Loading
             </div>
